79abf7e78584747ad2c7b2e48ffc805dc56adb85
zamaludin <zamaludin@live.com>
2020-04-18 14:40:34 +0700

    first

---
+++ iot-saklar-esp32.ino
@@ -0,0 +1,238 @@
+  #include <WiFi.h>
+#include <FirebaseESP32.h>
+#include <Servo.h>
+
+
+#define FIREBASE_HOST "newagent-19c5a.firebaseio.com" //Do not include https:// in FIREBASE_HOST
+#define FIREBASE_AUTH "QegVLrJWl6kNi4hyCQd0T2gcujq28F0KjB2ENlkJ"
+#define WIFI_SSID "Quroza"
+#define WIFI_PASSWORD "kayabersyukur"
+
+Servo myservo;  // create servo object to control a servo
+int pos = 0;
+
+//Define FirebaseESP32 data object
+FirebaseData firebaseData;
+unsigned long sendDataPrevMillis = 0;
+
+String path = "/Test/Stream";
+
+uint16_t count = 0;
+FirebaseJson json;
+
+void printResult(FirebaseData &data);
+
+void setup()
+{
+
+  Serial.begin(115200);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+  //Set database read timeout to 1 minute (max 15 minutes)
+  Firebase.setReadTimeout(firebaseData, 1000 * 60);
+  //tiny, small, medium, large and unlimited.
+  //Size and its write timeout e.g. tiny (1s), small (10s), medium (30s) and large (60s).
+  Firebase.setwriteSizeLimit(firebaseData, "tiny");
+
+  if (!Firebase.beginStream(firebaseData, path))
+    {
+      Serial.println("------------------------------------");
+      Serial.println("Can't begin stream connection...");
+      Serial.println("REASON: " + firebaseData.errorReason());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+  /*
+  This option allows get and delete functions (PUT and DELETE HTTP requests) works for device connected behind the
+  Firewall that allows only GET and POST requests.
+  
+  Firebase.enableClassicRequest(firebaseData, true);
+  */
+  myservo.attach(13);  // attaches the servo on pin 13 to the servo object
+  
+}
+
+
+
+void loop()
+{
+  
+
+  
+
+  if (!Firebase.readStream(firebaseData))
+  {
+    Serial.println("------------------------------------");
+    Serial.println("Can't read stream data...");
+    Serial.println("REASON: " + firebaseData.errorReason());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+
+  if (firebaseData.streamTimeout())
+  {
+    Serial.println("Stream timeout, resume streaming...");
+    Serial.println();
+  }
+
+  if (firebaseData.streamAvailable())
+  {
+    Serial.println("------------------------------------");
+    Serial.println("Stream Data available...");
+    Serial.println("STREAM PATH: " + firebaseData.streamPath());
+    Serial.println("EVENT PATH: " + firebaseData.dataPath());
+    Serial.println("DATA TYPE: " + firebaseData.dataType());
+    Serial.println("EVENT TYPE: " + firebaseData.eventType());
+    Serial.print("VALUE: ");
+    printResult(firebaseData);
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+}
+
+void printResult(FirebaseData &data)
+{
+
+  if (data.dataType() == "int"){
+    Serial.println(data.intData());
+    myservo.attach(13);
+    if (data.intData() == 1) {
+      for (pos = 50; pos <= 100; pos += 1) { // goes from 0 degrees to 180 degrees
+        // in steps of 1 degree
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(15);                       // waits 15ms for the servo to reach the position
+      }
+      for (pos = 100; pos <= 50; pos -= 1) { // goes from 0 degrees to 180 degrees
+        // in steps of 1 degree
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(15);                       // waits 15ms for the servo to reach the position
+      }
+    } else {
+      for (pos = 100; pos >= 50; pos -= 1) { // goes from 180 degrees to 0 degrees
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(15);                       // waits 15ms for the servo to reach the position
+      }
+      for (pos = 50; pos >= 100; pos += 1) { // goes from 180 degrees to 0 degrees
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(15);                       // waits 15ms for the servo to reach the position
+      }   
+    }
+    
+    myservo.detach();
+  }
+    
+  else if (data.dataType() == "float")
+    Serial.println(data.floatData(), 5);
+  else if (data.dataType() == "double")
+    printf("%.9lf\n", data.doubleData());
+  else if (data.dataType() == "boolean")
+    Serial.println(data.boolData() == 1 ? "true" : "false");
+  else if (data.dataType() == "string") {
+    Serial.println(data.stringData());
+    myservo.attach(13);
+    if (data.stringData() == "1") {
+      for (pos = 100; pos >= 50; pos -= 1) { // goes from 0 degrees to 180 degrees
+        // in steps of 1 degree
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(15);                       // waits 15ms for the servo to reach the position
+      }
+      for (pos = 50; pos <= 100; pos += 1) { // goes from 0 degrees to 180 degrees
+        // in steps of 1 degree
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(15);                       // waits 15ms for the servo to reach the position
+      }
+    } else {
+      for (pos = 100; pos <= 150; pos += 1) { // goes from 180 degrees to 0 degrees
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(15);                       // waits 15ms for the servo to reach the position
+      }
+      for (pos = 150; pos >= 100; pos -= 1) { // goes from 180 degrees to 0 degrees
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(15);                       // waits 15ms for the servo to reach the position
+      }   
+    }
+    
+    myservo.detach();
+  }
+    
+  else if (data.dataType() == "json")
+  {
+    Serial.println();
+    FirebaseJson &json = data.jsonObject();
+    //Print all object data
+    Serial.println("Pretty printed JSON data:");
+    String jsonStr;
+    json.toString(jsonStr, true);
+    Serial.println(jsonStr);
+    Serial.println();
+    Serial.println("Iterate JSON data:");
+    Serial.println();
+    size_t len = json.iteratorBegin();
+    String key, value = "";
+    int type = 0;
+    for (size_t i = 0; i < len; i++)
+    {
+      json.iteratorGet(i, type, key, value);
+      Serial.print(i);
+      Serial.print(", ");
+      Serial.print("Type: ");
+      Serial.print(type == JSON_OBJECT ? "object" : "array");
+      if (type == JSON_OBJECT)
+      {
+        Serial.print(", Key: ");
+        Serial.print(key);
+      }
+      Serial.print(", Value: ");
+      Serial.println(value);
+    }
+    json.iteratorEnd();
+  }
+  else if (data.dataType() == "array")
+  {
+    Serial.println();
+    //get array data from FirebaseData using FirebaseJsonArray object
+    FirebaseJsonArray &arr = data.jsonArray();
+    //Print all array values
+    Serial.println("Pretty printed Array:");
+    String arrStr;
+    arr.toString(arrStr, true);
+    Serial.println(arrStr);
+    Serial.println();
+    Serial.println("Iterate array values:");
+    Serial.println();
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+      Serial.print(i);
+      Serial.print(", Value: ");
+
+      FirebaseJsonData &jsonData = data.jsonData();
+      //Get the result data from FirebaseJsonArray object
+      arr.get(jsonData, i);
+      if (jsonData.typeNum == JSON_BOOL)
+        Serial.println(jsonData.boolValue ? "true" : "false");
+      else if (jsonData.typeNum == JSON_INT)
+        Serial.println(jsonData.intValue);
+      else if (jsonData.typeNum == JSON_DOUBLE)
+        printf("%.9lf\n", jsonData.doubleValue);
+      else if (jsonData.typeNum == JSON_STRING ||
+               jsonData.typeNum == JSON_NULL ||
+               jsonData.typeNum == JSON_OBJECT ||
+               jsonData.typeNum == JSON_ARRAY)
+        Serial.println(jsonData.stringValue);
+    }
+  }
+}
